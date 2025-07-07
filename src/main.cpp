38270445ba08bd922daf6ca177926b04f22ba998
minhnhatVA <minhnhat@wearevulcan.com>
2025-07-07 20:45:43 +0700

    Test Set Home and Movement

--- src/main.cpp
+++ src/main.cpp
@@ -1,75 +1,124 @@
-#include <Arduino.h>
-// #include <ESP32Servo.h>
-#include <ESP32PWM.h>
-#include "PWMStepper.h"
+#include "main.h"
 
-#define X_AXIS_LIMIT 13
-#define Y_AXIS_LIMIT 5
-#define Z_AXIS_LIMIT 23
 
-PWMStepper axisX(X_AXIS_PIN_DEFAULT, X_AXIS_DIR_DEFAULT, PWM_CHANNEL0, PWM_RESOLUTION_8);
-PWMStepper axisY(Y_AXIS_PIN_DEFAULT, Y_AXIS_DIR_DEFAULT, PWM_CHANNEL2, PWM_RESOLUTION_8);
+// PWMStepper axisX(X_AXIS_PIN_DEFAULT, X_AXIS_DIR_DEFAULT, PWM_CHANNEL0, PWM_RESOLUTION_8);
+// PWMStepper axisY(Y_AXIS_PIN_DEFAULT, Y_AXIS_DIR_DEFAULT, PWM_CHANNEL2, PWM_RESOLUTION_8);
 // PWMStepper axisZ(Z_AXIS_PIN_DEFAULT, Z_AXIS_DIR, PWM_CHANNEL2, PWM_FREQ, PWM_RESOLUTION);
 
+AccelStepper axisX(AccelStepper::DRIVER, X_AXIS_STEP, X_AXIS_DIR);
+AccelStepper axisY(AccelStepper::DRIVER, Y_AXIS_STEP, Y_AXIS_DIR);
+AccelStepper axisZ(AccelStepper::DRIVER, Z_AXIS_STEP, Z_AXIS_DIR);
 
-bool dirX = 0;
-bool dirY = 0;
+float stepsToMM(long mm){
+  long steps = (200 * 32) / (20 * 2); // 200 steps per revolution, 32 microsteps, 20 teeth pulley, 2 mm pitch
+  float stepsNeeded = mm * steps;     // Convert mm to steps
 
-
-void setup()
-{
-    Serial.begin(115200);
-    axisX.begin();
-    axisY.begin();
-
-    // pinMode(ENABLE_DRV_DEFAULT, OUTPUT);
-    // pinMode(X_AXIS_DIR_DEFAULT, OUTPUT);
-    // pinMode(Y_AXIS_DIR_DEFAULT, OUTPUT);
-    // pinMode(Z_AXIS_DIR_DEFAULT, OUTPUT);
-
-    pinMode(X_AXIS_LIMIT, INPUT_PULLUP);
-    pinMode(Y_AXIS_LIMIT, INPUT_PULLUP);
-    pinMode(Z_AXIS_LIMIT, INPUT_PULLUP);
-
-    // digitalWrite(ENABLE_DRV_DEFAULT, LOW);
-
-    // ledcSetup(PWM_CHANNEL0, 32000, PWM_RESOLUTION_8);
-    // ledcAttachPin(X_AXIS_PIN_DEFAULT, PWM_CHANNEL0); // Attach the X_AXIS to PWM channel 0
-
-    // ledcSetup(PWM_CHANNEL2, 32000, PWM_RESOLUTION_8);
-    // ledcAttachPin(Y_AXIS_PIN_DEFAULT, PWM_CHANNEL2); // Attach the Y_AXIS to PWM channel 0
-  axisY.moveDistanceCm(10.0, 160, 2000);  // Move 1 cm
+  return stepsNeeded; // Return the number of steps needed to move the specified distance in mm
 }
 
-void loop()
-{
+void homeAxis(AccelStepper axis, int ls) {
+  Serial.println("Homing...");
 
-    // if(digitalRead(X_AXIS_LIMIT) == LOW)
-    // {
-    //     Serial.printf("Stepper is Reached Limit \n");
-    //     axisX.stop();
-    //     dirX = !dirX;
-    //     delay(100);
-    // }
-    
-    // if(digitalRead(Y_AXIS_LIMIT) == LOW)
-    // {
-    //     Serial.printf("Stepper is Reached Limit \n");
-    //     axisY.stop();
-    //     dirY = !dirY;
-    //     delay(1000);
-    // }
-    // axisX.move(dirX, 12800);
+  axis.setMaxSpeed(1000);  // Slow speed for homing
+  axis.setAcceleration(500);
 
-    
-    // axisY.move(dirY, 800);
-    
-   
-    
+  // Move in negative direction until switch is hit
+  while (digitalRead(ls) == HIGH) {  // HIGH = not pressed
+    axis.moveTo(stepsToMM(axis.currentPosition() - 10));  // Keep moving slowly
+    axis.run();
+  }
 
-    // axisY.moveToLimit(dir, 12800, Y_AXIS_LIMIT);
+  // Stop motion
+  axis.stop();
+  delay(100);
 
-    delay(100);
+  // Optionally back off
+  axis.moveTo(stepsToMM(-1));  // Back off 1cm
+  while (axis.distanceToGo() != 0) {
+    axis.run();
+  }
+
+  // Set home coordinate
+  axis.setCurrentPosition(0);  // Now we're at position 0
+  Serial.println("Homing done!");
 }
 
+void setup(){
+  Serial.begin(115200);
 
+  pinMode(X_AXIS_LIMIT, INPUT_PULLUP);
+  pinMode(Y_AXIS_LIMIT, INPUT_PULLUP);
+  pinMode(Z_AXIS_LIMIT, INPUT_PULLUP);
+
+
+ 
+
+  
+  homeAxis(axisY, Y_AXIS_LIMIT); // Home Y axis
+  delay(100);
+  homeAxis(axisX, X_AXIS_LIMIT); // Home X axis
+  delay(100);
+
+  axisX.setMaxSpeed(100000);     // Set maximum speed for X axis
+  axisX.setAcceleration(50000); // Set acceleration for X axis
+
+  axisY.setMaxSpeed(100000);     // Set maximum speed for Y axis
+  axisY.setAcceleration(50000); // Set acceleration for Y axis
+
+
+  // digitalWrite(ENABLE_DRV_DEFAULT, LOW);
+  
+}
+
+void loop(){
+
+  axisX.moveTo(stepsToMM(0)); // Move X axis to 0 mm
+  axisX.runToPosition(); // Wait until the move is complete
+
+  axisY.moveTo(stepsToMM(0)); // Move Y axis to 0 mm
+  axisY.runToPosition(); // Wait until the move is complete
+  delay(2000);
+
+  axisX.moveTo(stepsToMM(100)); 
+  axisX.runToPosition(); 
+
+  axisY.moveTo(stepsToMM(100)); 
+  axisY.runToPosition(); 
+
+  /* 150 */
+  axisX.moveTo(stepsToMM(150)); 
+  axisX.runToPosition(); 
+  axisY.move(stepsToMM(50)); 
+  axisY.runToPosition(); 
+
+  /* 200 */
+  axisX.moveTo(stepsToMM(200)); 
+  axisX.runToPosition(); 
+  axisY.move(stepsToMM(50)); 
+  axisY.runToPosition(); 
+  
+  /* 300 */
+  axisX.moveTo(stepsToMM(300)); 
+  axisX.runToPosition(); 
+  axisY.move(stepsToMM(50)); 
+  axisY.runToPosition(); 
+  
+  /* 400 */
+  axisX.moveTo(stepsToMM(400)); 
+  axisX.runToPosition(); 
+  axisY.move(stepsToMM(50)); 
+  axisY.runToPosition(); 
+
+  axisX.moveTo(stepsToMM(500)); 
+  axisX.runToPosition(); 
+  axisY.move(stepsToMM(100)); 
+  axisY.runToPosition(); 
+  delay(1000); // Wait for a second before next iteration
+
+  axisX.moveTo(0);  // go back to home
+  axisY.moveTo(0);  // go back to home
+  while (axisX.distanceToGo() != 0 || axisY.distanceToGo() != 0) {
+    axisX.run();
+    axisY.run();
+  }  
+}
---
+++ src/main.h
@@ -0,0 +1,43 @@
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
+
+#include <Arduino.h>
+#include "PWMStepper.h"
+#include <AccelStepper.h>
+#include <math.h>
+#include <TMCStepper.h>
+
+
+/* NOTICE: DISCONNECT GPIO 0/2/5/12/15/RST FROM ENA PIN. IT WLL MAKE ESP32 CANNOT WORK AFTER FIRST TIME UPLOAD
+
+    + The GPIO 0/2/5/12/15/RST pin is Strapping Pin, which is used to select the boot mode of the ESP32.
+    + The ENA pin on the CNC Shield V3 is pull-up to 3v3 normally. This pin need to pull-down to GND to enable the driver.
+    + So that, if connect the GPIO12 to ENA pin, it will cause the ESP32 to enter the boot mode and cannot work normally.
+    + So, we need to disconnect the GPIO12 from ENA pin.
+    + If you want to use the ENA pin, you can use any other GPIO pin to control the ENA pin.
+    + The GPIO 12 is used to select the boot mode of the ESP32, so it should not be used as a normal GPIO pin.
+
+*/
+// #define ENABLE_DRV_DEFAULT 12  ~> 
+#define X_AXIS_STEP 26
+#define Y_AXIS_STEP 25
+#define Z_AXIS_STEP 17
+#define X_AXIS_DIR 16
+#define Y_AXIS_DIR 19
+#define Z_AXIS_DIR 14
+
+/* Limit Switch */
+#define X_AXIS_LIMIT 13
+#define Y_AXIS_LIMIT 5
+#define Z_AXIS_LIMIT 23
+
+#define CW 1
+#define CCW 0
+
+
+
+ 
+
+
+#endif // __MAIN_H__
